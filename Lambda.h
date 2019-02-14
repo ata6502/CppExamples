93ab6256e5667988d65228471c760e150f204188
Leszek Taratuta <ata6502@hotmail.com>
2019-02-13 22:51:26 -0500

    Update Lambda examples

--- Lambda.h
+++ Lambda.h
@@ -15,11 +15,12 @@ using std::string;
     a separate function or to write a function object.
 
     Use an explicit capture block for lambda expressions:
-    - Avoid using [&] and [=].
-      ??? Kate Gregory says: use [&] to avoid unnecessary copying.
-    - By default, captured variables are const.
+    - Avoid using [&] and [=]. Specify explicitly what variables you capture.
     - When you use [&] or [=] only variables used in lambda are captured.
-    - Examples:
+    - By default, captured variables are const.
+    - Default capture modes implicitly capture 'this'. You can only access class members by capturing 'this'
+
+    Examples:
         []     - captures nothing (no variables from the enclosing scope are captured)
         [i]    - captures i by value (makes a copy of i)
         [&j]   - captures j by reference (changes are reflected in the actual variable; dangling reference may be an issue when the original variable j goes out of scope
@@ -41,50 +42,40 @@ using std::string;
     By default, the captured variables are const. Use the mutable keyword if you need to modify the variables:
     - If a variable is captured by value, the local copy will be changed.
     - If a variable is captured by reference, the referenced variable will be changed.
+
+    default capture modes implicitly captures *this* and you can only access class members by capturing *this*
 */
 
 namespace LambdaExamples
 {
-    void LambdaInvocation()
+    void BasicLambda()
     {
-        // Simple lambda.
+        // Store a lambda in a variable.
         auto doubleValue = [](int z) { return z * 2; };
         cout << doubleValue(4) << " "; // 8
 
-        // Capture an external variable.
+        // Store a lambda in std::function
+        std::function<bool(double)> isGreaterThanZero = [](double d) { return d > 0.0; };
+        cout << isGreaterThanZero(1.0) << " "; // true
+
+        // A lambda in a for_each loop.
+        vector<int> v = { 1, 2, 3 };
+        std::for_each(v.begin(), v.end(),
+            [](int i) { cout << i; }); // lambda is handed each element of the vector: 1,2,3
+        cout << " ";
+    }
+
+    void CaptureVariables()
+    {
+        // Capture a variable by value.
         int x = 4;
         auto increaseValue = [x](int z) { return z + x; };
         cout << increaseValue(3) << " "; // 7
         x = 10; // change the variable
         cout << increaseValue(3) << " "; // still 7 because the variables are captured by value and x was 4 when it was captured: 3 + 4 = 7
-
-        //
-        // Create a lambda expression and immediately execute it.
-        //
-
-        // Simple lambda.
-        vector<int> v = { 1, 2, 3 };
-        std::for_each(v.begin(), v.end(),
-            [](int i) { cout << i; }); // lambda is handed each element of the vector: 1,2,3
-        cout << " ";
-
-        // Define a lambda expression without return type and without any parameters. 
-        // Note the parentheses () at the end, which cause the lambda to execute immediately.
-        [] { cout << "Hello "; }();
-
-        // Define a lambda that accepts a string argument and returns a string. Store the result
-        // in the variable 'result'. The return type can be omitted in this case.
-        // Notice the parentheses at the end of the lambda causing the lambda to be executed immediately.
-        string result = [](const string& s) -> string { return s; }("Aye!");
-        cout << result << " ";
-
-        // Store a pointer to a lambda expression and execute the lambda through the function pointer. 
-        auto fn = [](const string& s) { return s; };
-        cout << fn("Call1 ");
-        cout << fn("Call2 ");
     }
 
-    void ExplicitReturnType()
+    void SpecifyReturnType()
     {
         vector<int> v = { 1, 2, 3, 4 };
         vector<double> dv;
@@ -92,7 +83,8 @@ namespace LambdaExamples
         // Specify the return type explicitly. We need to specify the return type
         // because this lambda has multiple returns.
         // std::transform transforms a collection into another collection.
-        // std::back_inserter inserts transfomed items at the back of a collection. To insert transfomed items at the beginning of the collection, use begin(dv) instead.
+        // std::back_inserter inserts transfomed items at the back of a collection. 
+        // To insert transfomed items at the beginning of the collection, use begin(dv) instead.
         std::transform(v.begin(), v.end(), std::back_inserter(dv),
             [](int n) -> double
         {
@@ -115,12 +107,30 @@ namespace LambdaExamples
         };
     }
 
-    /*
-        If variables from the enclosing scope are captured by value, a copy of those variables will become available
-        in the body of the lambda expression. Those copies are const by default. If the lambda expression is marked 
-        as mutable, the copies are not const and the lambda body can modify the local copies.
-    */
-    void MutableLambda()
+    std::function<int(void)> ReturnLambda(int x)
+    {
+        // int(void) is a function accepting no arguments and returning an integer.
+        return [x]()->int {return 2 * x; }; // the same as return [x]{return 2*x;};
+    }
+
+    void SetLambda(const std::function<bool(double)>& lambda)
+    {
+        // store the lambda in a variable of type function<bool(double)>
+    }
+
+    void ReturnAndSetLambda()
+    {
+        // Create a lambda expression of type std::function<int(void)>
+        auto multiplier = ReturnLambda(3);
+        cout << multiplier() << " "; // 6
+
+        // Pass a lambda as a param.
+        SetLambda([](double d) { return d > 0.0; });
+    }
+
+    // If the lambda expression is marked as mutable, the copies of captured 
+    // variables are not const and the lambda body can modify the local copies.
+    void MakeLambdaMutable()
     {
         vector<int> v = { 1, 2, 3, 4 };
 
@@ -170,32 +180,111 @@ namespace LambdaExamples
         cout << "-" << n << " ";
     }
 
-    std::function<int(void)> ReturnLambda(int x)
+    // Non-local variables can always be accessed in a lambda.
+    std::function<bool()> AccessNonLocalVariables()
     {
-        // int(void) is a function accepting no arguments and returning an integer.
-        return [x]()->int {return 2 * x; }; // the same as return [x]{return 2*x;};
+        static auto a = 5;
+        static auto b = -3;
+        return []() { return a + b > 0; };
     }
 
-    void SetLambda(const std::function<bool(double)>& lambda)
+    void ExecuteImmediately()
     {
-        // store the lambda in a variable of type function<bool(double)>
+        // The parentheses () at the end of a lambda cause the lambda to execute immediately.
+
+        // Define a lambda without return type and without any parameters. 
+        [] { cout << "A "; }();
+
+        // Define a lambda that accepts a string argument and returns a string. 
+        // The return type can be omitted in this case.
+        string result = [](const string& s) -> string { return s; }("Aye!");
+        cout << result << " ";
     }
 
-    void ReturnAndSetLambda()
+    void ConvertToFunctionPointer()
     {
-        // Create a lambda expression of type std::function<int(void)>
-        auto multiplier = ReturnLambda(3);
-        cout << multiplier() << " "; // 6
-
-        // Pass a lambda as a param.
-        SetLambda([](double d) { return d > 0.0; });
+        // Convert a lambda to a function pointer.
+        typedef int(*Func)();
+        Func f = [] { return 10; };
+        f(); // invoke lambda via function pointer
     }
 
-    void Lambda()
+    struct Order { int Number; };
+    struct Customer { string Name; vector<Order> Orders; };
+
+    void LambdaNesting()
     {
-        LambdaInvocation();
-        ExplicitReturnType();
-        MutableLambda();
+        vector<Customer> customers;
+        customers.push_back(Customer{ "A", { {1},{2} } });
+        customers.push_back(Customer{ "B", { {3},{4} } });
+        customers.push_back(Customer{ "C", { {5},{6} } });
+
+        for_each(begin(customers), end(customers),
+            [=](const Customer& c)
+        {
+            cout << c.Name << ":";
+
+            for_each(begin(c.Orders), end(c.Orders), 
+                [=](const Order& o)
+            {
+                cout << o.Number;
+            });
+
+            cout << " ";
+        });
+    }
+
+    void EmulateRecursion()
+    {
+        // recursion emulation via std::function
+        std::function<int(int)> fibonacci = [&](int n) -> int
+        {
+            if (n < 1)
+                return -1;
+            else if (n == 1 || n == 2)
+                return 1;
+            else
+                return fibonacci(n - 1) + fibonacci(n - 2);
+        };
+
+        cout << "Fib(7)=" << fibonacci(7) << " ";
+    }
+
+    void UndefinedBehaviour()
+    {
+        {
+            std::function<int()> f;
+            {
+                auto i = 5;
+                f = [&i] { return i; }; // i is caught by reference
+            }
+            auto i = 10;
+            f(); // undefined because i is out of scope
+        }
+
+        {
+            std::function<int()> f;
+            {
+                auto p = new int(10);
+                f = [=] { return *p; };
+                delete p;
+            }
+            f(); // undefined behavior because p has been deleted
+        }
+    }
+
+    void Test()
+    {
+        BasicLambda();
+        CaptureVariables();
+        SpecifyReturnType();
         ReturnAndSetLambda();
+        MakeLambdaMutable();
+        AccessNonLocalVariables()(); // AccessNonLocalVariables returns a lambda; the second parentheses cause the lambda to execute immediately
+        ExecuteImmediately();
+        ConvertToFunctionPointer();
+        LambdaNesting();
+        EmulateRecursion();
+        UndefinedBehaviour();
     }
 }
